60af6543b77e308c5379b22e738773f3f855ef62
Aryan Kashem <aryankashem@gmail.com>
2024-06-09 01:09:53 -0400

    [CI PIPELINE] Fixed check_format.sh script to prevent cpputest directory from being read

--- scripts/check_format.sh
+++ scripts/check_format.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FILES=$(find . -name "*.c" -o -name "*.h" -not -path "./cpputest/*")
+FILES=$(find . -type f \( -name "*.c" -o -name "*.h" \) -not -path "./cpputest/*")
 
 format_check=0
 
